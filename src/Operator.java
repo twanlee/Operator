8394322c5be37fcc91aa3f08dee8c2681964493b
unknown <levantuan6898@gmail.com>
2020-04-08 17:47:09 +0700

    add file

---
+++ src/Operator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Operator {
+    public static void main(String[] args) {
+        float width, height;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter width: ");
+        width = scanner.nextFloat();
+
+        System.out.println("Enter height: ");
+        height = scanner.nextFloat();
+        float area = width*height;
+        System.out.println("Area is: " + area);
+    }
+}
